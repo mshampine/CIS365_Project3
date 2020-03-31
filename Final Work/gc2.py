ff048a185f5b103b4d4595a9a01e9960a995044d
Nabeel V <nabvali98@gmail.com>
2020-03-31 11:13:52 -0400

    Added commented/formatted files

---
+++ Final Work/gc2.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python3
+
+import cv2 as cv
+import signal
+import sys
+import tensorflow.keras
+import numpy as np
+import pyautogui
+
+"""
+Project 3 : CIS365-01
+Title: Gesture ANN Camera
+Description:
+Date: 2/29/2020
+Authors: Matt Shampine, Nabeel Vali
+"""
+
+np.set_printoptions(suppress=True)
+
+# Load our trained model to be able to make predictions on image input
+model = tensorflow.keras.models.load_model('my_model99.h5')
+
+
+def sig_handler():
+    """
+    Handles CTRL-C interrupt to cleanly terminate the program
+    :return: None
+    """
+    print("\nNow closing resources.")
+    cam.release()
+    cv.destroyAllWindows()
+    exit(0)
+
+
+class camera():
+    """
+    Initializes the users camera via OpenCV, passes frames from the camera into
+    the trained model to generate a gesture prediction. The prediction is used
+    to control what computer action is taken.
+    """
+
+    def __init__(self, camNum=0):
+        """
+        Explain what this method does
+        :param camNum:
+        """
+
+        # Global - so the sighandler can close resources upon ^C.
+        global cam
+        cam = cv.VideoCapture(camNum)
+
+        # Comment this block
+        self.width = cam.get(cv.CAP_PROP_FRAME_WIDTH)
+        self.height = cam.get(cv.CAP_PROP_FRAME_HEIGHT)
+        # print("w:", self.width, "h:", self.height)
+        self.center_x = (int(self.width / 2))
+        self.center_y = (int(self.height / 2))
+        self.center = (self.center_x, self.center_y)
+        self.txt_center = (self.center_x - 75, self.center_y + 230)
+
+        # Comment this block
+        self.box_x1 = self.center_x - 125
+        self.box_y1 = self.center_y - 180
+        self.pos1 = (self.box_x1, self.box_y1)
+        self.box_x2 = self.center_x + 125
+        self.box_y2 = self.center_y + 180
+        self.pos2 = (self.box_x2, self.box_y2)
+
+        # Class level flags to keep track of detected gestures
+        self.handFlag = 0
+        self.fistFlag = 0
+
+        # Note: (G, B, R) not (R, G, B).
+        self.red = (0, 0, 255)
+        self.green = (0, 255, 0)
+        self.run()
+
+    def run(self):
+        """
+        This method executes on program start, it loops infinitely until user interruption.
+        A connection to the webcam is created and frames are passed to the model for a
+        prediction. Helper methods are then called to generate computer actions and illustrations
+        within the application window.
+        :return: None
+        """
+        while True:
+            ret_val, frame = cam.read()
+
+            if ret_val == False:
+                print("Error reading frame, webcam may be disconnected.")
+                exit()
+
+            # Save an unedited frame for CNN consumption.
+            saveFrame = frame.copy()
+
+            # Resize the input frame to 224x224, this size needed by the model
+            frame1 = cv.resize(saveFrame, (224, 224))
+            cv.imshow('Input Frame', frame1)
+
+            # Reshape the frame into a 3 dimensional array that can be understood by the CNN
+            frame1 = frame1.reshape(1, 224, 224, 3)
+            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+            image_array = np.asarray(frame1)
+
+            # Normalize/Down-sample our image, then pass it into the model an an array
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+            data[0] = normalized_image_array
+            prediction = model.predict(data)
+
+            #Predictions are returned in array, grab the index of the max prediction
+            maxVal = np.argmax(prediction)
+
+            # if maxVal == 2:
+            # print('hit')
+            # time.sleep(10)
+            # pyautogui.hotkey('ctrl', 'c')
+
+            print(prediction)
+
+            #Comment Here
+            if maxVal > 0:
+                self.overlayRect(frame, True, maxVal)
+            else:
+                self.overlayRect(frame, False, maxVal)
+
+            cv.imshow('feed', frame)
+
+            # Press ESC to quit.
+            if cv.waitKey(1) == 27:
+                break
+
+        # Close resources.
+        cam.release()
+        cv.destroyAllWindows()
+
+    def overlayRect(self, frame, color, gest):
+        """
+        Describe The method
+        :param frame:
+        :param color:
+        :param gest:
+        :return: None
+        """
+        set_color = self.green
+        text = ""
+        if color and gest == 1:
+            text = "Hand"
+            self.moveHand()
+        elif color and gest == 2:
+            text = " Fist"
+            self.moveFist()
+        else:
+            set_color = self.red
+
+            # Reset the gesture detected flags to 0
+            self.handFlag = 0
+            self.fistFlag = 0
+        # Draw center circle.
+        cv.circle(frame, self.center, 8, set_color, -1)
+        # Draw ROI (region of interest) on frame.
+        cv.rectangle(frame, self.pos1, self.pos2, set_color, 3)
+        cv.putText(frame, text, self.txt_center, cv.FONT_HERSHEY_SIMPLEX, 2, self.green, 2)
+
+    def moveHand(self):
+        """
+        When a hand is detected, this method calls PyAutoGUI to imitate
+        a keyboard press. 'space' is pressed and handFlag is set to true.
+        :return: None
+        """
+        if self.handFlag != 1:
+            pyautogui.press('space')
+            self.handFlag = 1
+
+    def moveFist(self):
+        """
+        When a fist is detected, this method calls PyAutoGUI to imitate
+        a keyboard press. 'Ctrl-Right' is skip a song in Spotify.
+        FistFlag is set to true.
+        :return:
+        """
+        if self.fistFlag != 1:
+            pyautogui.hotkey('ctrl', 'right')
+            self.fistFlag = 1
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, sig_handler)
+    if len(sys.argv) == 2:
+        if sys.argv[1].isdigit() and int(sys.argv[1]) == 1:
+            test = camera(1)
+        else:
+            print("Enter 1 to enable another camera besides default.")
+    else:
+        test = camera()
---
+++ Final Work/test2.py
@@ -0,0 +1,89 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+import tensorflow as tf
+import os
+
+"""
+Project 3 : CIS365-01
+Title: Gesture ANN Camera
+Description:
+Date: 2/29/2020
+Authors: Matt Shampine, Nabeel Vali
+
+This file hold the model and its training code
+"""
+
+# Establish the base directory location for all the images
+base_dir = './Images'
+
+# Append the test and training directory paths to the base directory
+train_dir = os.path.join(base_dir, 'Train')
+test_dir = os.path.join(base_dir, 'Test')
+
+# Append each classes training path to the base training path
+train_FlatHand_dir = os.path.join(train_dir, "FlatHand")
+train_Fist_dir = os.path.join(train_dir, "Fist" )
+train_BlankWall_dir = os.path.join(train_dir, "BlankWall")
+
+# Append each classes testing path to the base test path
+test_FlatHand_dir = os.path.join(test_dir, "FlatHand")
+test_Fist_dir = os.path.join(test_dir, "Fist")
+test_BlankWall_dir = os.path.join(test_dir, "BlankWall")
+
+
+# Rescale our data from 0 - 255 to 0 - 1, for use in the model.
+train_datagen = ImageDataGenerator( rescale = 1.0/255.)
+test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+# Generate batches of training and testing data from the batches created above
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=10,
+                                                    class_mode='categorical',
+                                                    target_size=(224,224),
+                                                    shuffle=True)
+
+test_generator = test_datagen.flow_from_directory(test_dir,
+                                                    batch_size=10,
+                                                    class_mode='categorical',
+                                                    target_size=(224,224),
+                                                    shuffle=True)
+
+
+# Build a CNN model by sequentially adding the various hidden layers.
+# Convolution layers build a feature map out of our image input, it also scales the image down
+# Max pooling layers further downsample our image input
+# Dropout layers, with a certain probability, discard some sample data to prevent training on noise
+# Flatten() compresses an input before feeding it into the dense layer
+# Dense layers are used to aggregate the model data and form a prediction
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(224,224,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu', kernel_initializer='he_uniform'),
+    tf.keras.layers.Dense(4, activation="softmax")
+])
+
+# Optimizer used: Stochastic Gradient Descent
+# We set a higher momentum to increase the likelihood of convergence on an optimal point
+optimizer = SGD(lr=0.001, momentum=0.9)
+
+model.compile(optimizer=optimizer,
+               loss="categorical_crossentropy",
+               metrics=['accuracy'])
+
+# Train the model using the training data, validate it with our test data
+model.fit(train_generator,
+      shuffle=True,
+      epochs=5,
+      validation_data=test_generator,
+      verbose=1,
+)
+
+
+#model.save('finalModel.h5')
